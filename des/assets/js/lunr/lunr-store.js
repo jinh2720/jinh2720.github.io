3ef46d38e075378f74e7f150044a97773dcd6f1b
jinh2720 <jinh2720@gmail.com>
2021-04-07 07:32:57 +0900

    okay

---
+++ des/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "다섯번째",
+        "excerpt":"안녕하세요. 5번째 게시글입니다. 5  ","categories": [],
+        "tags": [],
+        "url": "/categories/Android/",
+        "teaser": null
+      },{
+        "title": "첫 포스팅",
+        "excerpt":"처음 블로그 게시물   내용입니다.~  ","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      },{
+        "title": "일곱번째",
+        "excerpt":"7번째  ","categories": [],
+        "tags": [],
+        "url": "/categories/iOS/seven/",
+        "teaser": null
+      },{
+        "title": "Posts by Category",
+        "excerpt":"여섯번째입니다. 6  ","categories": [],
+        "tags": [],
+        "url": "/categories/Android/",
+        "teaser": null
+      },{
+        "title": "네 번째",
+        "excerpt":"안녕하세요. 네 번째 게시글입니다. 4s  ","categories": [],
+        "tags": [],
+        "url": "/categories/iOS/",
+        "teaser": null
+      },{
+        "title": "두번째",
+        "excerpt":"안녕하세요 두 번째 포스팅입나다.  ","categories": [],
+        "tags": [],
+        "url": "/categories/iOS/",
+        "teaser": null
+      },{
+        "title": "세 번째",
+        "excerpt":"세번째입니다.  ","categories": [],
+        "tags": [],
+        "url": "/categories/Server/",
+        "teaser": null
+      }]
