b1cee63c3d3ac15fd0ae1bd88cd6902dc499c2da
jinh2720 <jinh2720@gmail.com>
2021-04-08 07:55:37 +0900

    he

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source "https://rubygems.org"
-gemspec
-gem "github-pages", group: :jekyll_plugins
-gem "jekyll-include-cache", group: :jekyll_plugins
+gem "json"
+gem "jekyll"
+gem "jekyll-sitemap"
+gem "jekyll-feed"
+gem "jekyll-paginate"
+gem "jekyll-gist"
+
+gem 'jekyll-include-cache' # 추가
--- des/assets/js/lunr/lunr-store.js
+++ des/assets/js/lunr/lunr-store.js
@@ -1,43 +1,49 @@
 var store = [{
         "title": "다섯번째",
-        "excerpt":"안녕하세요. 5번째 게시글입니다. 5  ","categories": [],
+        "excerpt":"안녕하세요. 5번째 게시글입니다. 5  ","categories": ["programming","python"],
         "tags": [],
-        "url": "/categories/Android/",
+        "url": "/programming/python/fifth/",
         "teaser": null
       },{
         "title": "첫 포스팅",
-        "excerpt":"처음 블로그 게시물   내용입니다.~  ","categories": [],
+        "excerpt":"처음 블로그 게시물   내용입니다.~  ","categories": ["programming","python"],
         "tags": [],
-        "url": "/first/",
+        "url": "/programming/python/first/",
         "teaser": null
       },{
         "title": "일곱번째",
-        "excerpt":"7번째  ","categories": [],
+        "excerpt":"7번째  ","categories": ["programming","python"],
         "tags": [],
-        "url": "/categories/iOS/seven/",
+        "url": "/programming/python/seventh/",
         "teaser": null
       },{
-        "title": "Posts by Category",
-        "excerpt":"여섯번째입니다. 6  ","categories": [],
+        "title": "6번째",
+        "excerpt":"여섯번째입니다. 6  ","categories": ["programming","java"],
         "tags": [],
-        "url": "/categories/Android/",
+        "url": "/programming/java/sixth/",
         "teaser": null
       },{
         "title": "네 번째",
-        "excerpt":"안녕하세요. 네 번째 게시글입니다. 4s  ","categories": [],
+        "excerpt":"안녕하세요. 네 번째 게시글입니다. 4s  ","categories": ["programming","javascript"],
         "tags": [],
-        "url": "/categories/iOS/",
+        "url": "/programming/javascript/fourth/",
         "teaser": null
       },{
         "title": "두번째",
-        "excerpt":"안녕하세요 두 번째 포스팅입나다.  ","categories": [],
+        "excerpt":"안녕하세요 두 번째 포스팅입나다.  ","categories": ["programming","java"],
         "tags": [],
-        "url": "/categories/iOS/",
+        "url": "/programming/java/second/",
         "teaser": null
       },{
         "title": "세 번째",
-        "excerpt":"세번째입니다.  ","categories": [],
+        "excerpt":"세번째입니다.  ","categories": ["programming","javascript"],
         "tags": [],
-        "url": "/categories/Server/",
+        "url": "/programming/javascript/third/",
+        "teaser": null
+      },{
+        "title": "Exam",
+        "excerpt":"내용입니다.   ","categories": ["programming","python"],
+        "tags": [],
+        "url": "/programming/python/exam/",
         "teaser": null
       }]
